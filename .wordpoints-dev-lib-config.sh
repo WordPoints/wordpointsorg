4269a0969e31a44427f010b3d75a23b9e95f524b
JDGrimes <jdg@codesymphony.co>
2016-12-05 16:32:17 -0500

    Capitalize HTTP
    So that it isn't picked up and flagged itself.

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -9,7 +9,7 @@ function wordpoints-dev-lib-config() {
 	# Exclude WP HTTP TestCase cache files from the disallowed strings check.
 	CODESNIFF_PATH_STRINGS=("${CODESNIFF_PATH_STRINGS[@]}" '!' -path './tests/phpunit/cache/wp-http-tc/*')
 
-	# Ignore lines that currently still need to have 'http' in them.
+	# Ignore lines that currently still need to have 'HTTP' in them.
 	CODESNIFF_IGNORED_STRINGS=("${CODESNIFF_IGNORED_STRINGS[@]}" -e http://example.org -e 'this->url')
 }
 
