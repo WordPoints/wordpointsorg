2b7705532bf2c586809fcbfb680025ad2af7838c
JDGrimes <jdg@codesymphony.co>
2016-12-05 16:31:49 -0500

    Stop overriding WPCS version
    I don't think that this is necessary anymore.

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -3,9 +3,6 @@
 # Sets up custom configuration.
 function wordpoints-dev-lib-config() {
 
-	# Use the develop branch for WPCS.
-	export WPCS_GIT_TREE=develop
-
 	# This needs to be set for caching to work.
 	export WP_HTTP_TC_HOST=example.com
 
